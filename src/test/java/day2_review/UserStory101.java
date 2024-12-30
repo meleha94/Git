931f8918e178a6584c706062b16710945ef3aefb
Meleha <meleha.yousafit@gmail.com>
2024-12-30 16:12:22 -0500

    Pushing together

---
+++ src/test/java/day2_review/UserStory101.java
@@ -0,0 +1,8 @@
+package day2_review;
+
+public class UserStory101 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Recapping like day 1, I wanted to recap more ");
+    }
+}
